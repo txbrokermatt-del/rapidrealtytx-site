0afcdffa8efa9ee99862953ab4e1c55d253dcb51
txbrokermatt-del <txbrokermatt@gmail.com>
2026-02-05 16:56:17 -0600

    Add menu toggle functionality to site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,8 @@
+(function () {
+  const btn = document.querySelector('[data-menu-btn]');
+  const menu = document.querySelector('[data-menu]');
+  if (btn && menu) {
+    btn.addEventListener('click', () => menu.classList.toggle('open'));
+  }
+})();
 
